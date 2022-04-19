e65e4f60c730c8346f2ead2b5583214173bf5c10
danieelfcr <danielfcab2002@gmail.com>
2022-04-19 17:15:14 -0600

    Project creation

---
+++ LAB05-ED1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LAB05_ED1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
