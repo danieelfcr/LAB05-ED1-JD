1e28e2e8dff5bf70d321289ecb43aeb8b56aaef5
danieelfcr <danielfcab2002@gmail.com>
2022-04-20 00:50:22 -0600

    Create by CSV implemented (insertion is missing)

--- LAB05-ED1/Controllers/TwoThreeController.cs
+++ LAB05-ED1/Controllers/TwoThreeController.cs
@@ -1,22 +1,85 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 using System.Threading.Tasks;
+using ClassLibrary;
+using LAB05_ED1.Helpers;
 
 namespace LAB05_ED1.Controllers
 {
     public class TwoThreeController : Controller
     {
-        // GET: TwoThreeController
-        public ActionResult Index()
+        private IWebHostEnvironment Environment;
+        public TwoThreeController(IWebHostEnvironment _environment)
         {
+            Environment = _environment;
+        }
+
+        // GET: TwoThreeController
+        public ActionResult Index(List<Vehicle> list)
+        {
+            return View(list);
+        }
+
+        [HttpPost]
+        public ActionResult Index(IFormFile File)
+        {
+            if (File != null)
+            {
+
+                ViewBag.File = true;
+                string path = Path.Combine(this.Environment.WebRootPath, "Uploads");
+
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+
+                string FileName = Path.GetFileName(File.FileName);
+                string filePath = Path.Combine(path, FileName);
+                FileStream stream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+
+
+
+                string DataList = System.IO.File.ReadAllText(filePath);
+
+                DataList.Trim();
+                StreamReader read = new StreamReader(filePath);
+
+                try
+                {
+                    string line = "";
+                    while ((line = read.ReadLine()) != null)
+                    {
+                        string[] aux = line.Split(",");
+                        Vehicle vehicle = new Vehicle
+                        {
+                            LicensePlate = aux[0],
+                            Color = aux[1],
+                            Owner = aux[2],
+                            Latitude = Convert.ToDouble(aux[3]),
+                            Longitude = Convert.ToDouble(aux[4])
+                        };
+
+                        //INSERTION IS MISSING
+
+                    }
+                }
+                catch
+                {
+                    return Content("There was an error with the file you're trying to import");
+                }
+            }
+
             return View();
         }
 
-        // GET: TwoThreeController/Details/5
-        public ActionResult Details(int id)
+            // GET: TwoThreeController/Details/5
+            public ActionResult Details(int id)
         {
             return View();
         }
@@ -24,7 +87,7 @@ namespace LAB05_ED1.Controllers
         // GET: TwoThreeController/Create
         public ActionResult Create()
         {
-            return View();
+            return View(new Vehicle());
         }
 
         // POST: TwoThreeController/Create
@@ -34,6 +97,17 @@ namespace LAB05_ED1.Controllers
         {
             try
             {
+                Vehicle vehicle = new Vehicle()
+                {
+                    LicensePlate = collection["LicensePlate"],
+                    Color = collection["Color"],
+                    Owner = collection["Owner"],
+                    Latitude = Convert.ToDouble(collection["Latitude"]),
+                    Longitude = Convert.ToDouble(collection["Longitude"]),
+                };
+
+                //Insertion in tree is missing
+
                 return RedirectToAction(nameof(Index));
             }
             catch
