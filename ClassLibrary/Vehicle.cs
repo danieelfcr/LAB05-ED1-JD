9c6021cf4a5e7d38971020cd5a38beaef767d973
danieelfcr <danielfcab2002@gmail.com>
2022-04-19 23:03:44 -0600

    ClassLibrary and Vehicle Class created

---
+++ ClassLibrary/Vehicle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ClassLibrary
+{
+    public class Vehicle
+    {
+        [Required]
+        public String LicensePlate { get; set; }
+        [Required]
+        public String Color { get; set; }
+        [Required]
+        public String Owner { get; set; }
+        [Required]
+        [Range(-90, 90)]
+        public Double Latitude { get; set; }
+        [Required]
+        [Range(-180, 180)]
+        public Double Longitude { get; set; }
+
+    }
+}
